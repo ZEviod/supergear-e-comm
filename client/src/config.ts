765d4a00a9a30ca2bf9be053471fe5e59ca16e2b
ViveK Poonia <vivekpoonia2112@gmail.com>
2024-07-19 22:41:38 +0530

    local error resolved

--- client/src/config.ts
+++ client/src/config.ts
@@ -6,7 +6,7 @@ const checkConfig = (server: string): Config | {} => {
   switch (server) {
     case "production":
       config = {
-        baseUrl: "https://supergearyt.vercel.app/",
+        baseUrl: "",
       };
       break;
     case "local":
@@ -20,5 +20,6 @@ const checkConfig = (server: string): Config | {} => {
   return config;
 };
 
-export const selectServer = "production";
+// export const selectServer = "production";
+export const selectServer = "local";
 export const config = checkConfig(selectServer) as Config;
