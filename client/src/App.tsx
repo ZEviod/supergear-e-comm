6e1d60840aa8ffac1ffe53be68218410a1e9cb30
ViveK Poonia <vivekpoonia2112@gmail.com>
2024-07-19 21:38:05 +0530

    dynamic pages

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,25 @@
+import "react-multi-carousel/lib/styles.css";
+import BannerCategories from "./ui/BannerCategories";
+import HomeBanner from "./ui/HomeBanner";
+import Hightlights from "./ui/Hightlights";
+import Categories from "./ui/Categories";
+import ProductList from "./ui/ProductList";
+import DiscountedBanner from "./ui/DiscountedBanner";
+import Blog from "./ui/Blog";
+
 function App() {
   return (
     <main>
-      <p>Ecomm demo</p>
+      <BannerCategories />
+      <HomeBanner />
+      <Hightlights />
+      <Categories />
+      {/* ProductList */}
+      <ProductList />
+      {/* DiscountedBanner */}
+      <DiscountedBanner />
+      {/* Blog */}
+      <Blog />
     </main>
   );
 }
---
+++ client/src/ui/AddToCartBtn.tsx
@@ -0,0 +1,121 @@
+import { twMerge } from "tailwind-merge";
+import { ProductProps } from "../type";
+// import { store } from "../lib/store";
+import toast from "react-hot-toast";
+import { useEffect, useState } from "react";
+import { FaMinus, FaPlus } from "react-icons/fa";
+import PriceTag from "./PriceTag";
+
+const AddToCartBtn = ({
+  className,
+  title,
+  product,
+  showPrice = true,
+}: {
+  className?: string;
+  title?: string;
+  product?: ProductProps;
+  showPrice?: boolean;
+}) => {
+  const [existingProduct, setExistingProduct] = useState<ProductProps | null>(
+    null
+  );
+  // const { addToCart, cartProduct, decreaseQuantity } = store();
+
+  // useEffect(() => {
+  //   const availableItem = cartProduct.find(
+  //     (item: any) => item?._id === product?._id
+  //   );
+
+  //   setExistingProduct(availableItem || null);
+  // }, [product, cartProduct]);
+
+  // const handleAddToCart = () => {
+  //   if (product) {
+  //     addToCart(product);
+  //     toast.success(`${product?.name.substring(0, 10)} added successfully!`);
+  //   } else {
+  //     toast.error("Product is undefined!");
+  //   }
+  // };
+
+  const handleDeleteProduct = () => {
+    if (existingProduct) {
+      if (existingProduct?.quantity > 1) {
+        // decreaseQuantity(existingProduct?._id);
+        toast.success(
+          `${product?.name.substring(0, 10)} decreased successfully`
+        );
+      } else {
+        toast.error("You can not decrease less than 1");
+      }
+    } else {
+    }
+  };
+
+  const newClassName = twMerge(
+    "bg-[#f7f7f7] uppercase text-xs py-3 text-center rounded-full font-semibold hover:bg-black hover:text-white hover:scale-105 duration-200 cursor-pointer",
+    className
+  );
+
+  const getRegularPrice = () => {
+    if (existingProduct) {
+      if (product) {
+        return product?.regularPrice * existingProduct?.quantity;
+      }
+    } else {
+      return product?.regularPrice;
+    }
+  };
+
+  const getDiscountedPrice = () => {
+    if (existingProduct) {
+      if (product) {
+        return product?.discountedPrice * product?.quantity;
+      }
+    } else {
+      return product?.discountedPrice;
+    }
+  };
+
+  return (
+    <>
+      {showPrice && (
+        <div>
+          <PriceTag
+            regularPrice={getRegularPrice()}
+            discountedPrice={getDiscountedPrice()}
+          />
+        </div>
+      )}
+      {existingProduct ? (
+        <div className="flex self-center items-center justify-center gap-2">
+          <button
+            onClick={handleDeleteProduct}
+            className="bg-[#f7f7f7] text-black p-2 border-[1px] border-gray-200 hover:border-skyText rounded-full text-sm hover:bg-white duration-200 cursor-pointer"
+          >
+            <FaMinus />
+          </button>
+          <p className="text-base font-semibold w-10 text-center">
+            {existingProduct?.quantity}
+          </p>
+          <button
+            // onClick={handleAddToCart}
+            className="bg-[#f7f7f7] text-black p-2 border-[1px] border-gray-200 hover:border-skyText rounded-full text-sm hover:bg-white duration-200 cursor-pointer"
+          >
+            <FaPlus />
+          </button>
+        </div>
+      ) : (
+        <button
+          // onClick={handleAddToCart}
+          className={newClassName}
+        >
+          {title ? title : "Add to cart"}
+        </button>
+      )}
+    </>
+  );
+};
+
+export default AddToCartBtn;
